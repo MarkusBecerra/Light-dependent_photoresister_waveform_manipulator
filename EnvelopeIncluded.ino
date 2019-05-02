14e41b1ecf44c9ce88e602f17a0b61ff9f5a2bc5
MarkusB <markusbecerra@gmail.com>
2019-05-01 20:45:31 -0500

    EnvelopeCode
    coolbeans its at the bottom of envelopeincluded

---
+++ EnvelopeIncluded.ino
@@ -0,0 +1,152 @@
+#include <DueTimer.h>
+#include <SineWaveDue.h>
+
+int lightCal;
+int lightVal;
+
+int buttonState = 0;
+int lastButtonState = buttonState;
+
+int state = 0; // 0 = square, 1 = sine, 2 = sawtooth
+const int ph1 = A0;
+int sensitivity = 100;
+
+int hertz = 400;
+double delayTime = 1000000 / (hertz * 2);
+int playHertz = hertz;
+      
+void setup()  { 
+  analogReadResolution(10);
+  analogWriteResolution(10);
+  Serial.begin(9600);
+  lightCal = analogRead(ph1);
+  pinMode(7, INPUT);
+  
+}
+
+void loop()  {
+  lightVal = analogRead(ph1);
+  buttonState = digitalRead(7);
+  hertz = analogRead(A1);
+  delayTime = 1000000 / (hertz * 2);
+
+  // DETERMINE BUTTON PUSH
+  if(buttonState != lastButtonState){
+    if(buttonState == 1){
+      if(state == 2)
+        state = 0;
+      else
+        state++; 
+    }
+    lastButtonState = buttonState;
+    delay(50);
+  }
+  
+  // PLAY SOUNDS
+  if(state == 0){
+    if(lightVal < lightCal - sensitivity){
+      analogWrite(DAC0, 1023);
+      delayMicroseconds(delayTime);
+      analogWrite(DAC0, 0);
+      delayMicroseconds(delayTime);
+    }
+  }
+  else if(state == 1){
+    if(lightVal < lightCal - sensitivity){
+      sw.playTone(hertz);
+      playHertz = hertz;
+      
+      while(lightVal < lightCal - sensitivity && hertz <= playHertz + 10 && hertz >= playHertz - 10){
+        lightVal = analogRead(ph1);
+        hertz = analogRead(A1);
+      }
+      sw.stopTone();
+    }
+  }
+  else if(state == 2){
+    Serial.println("Sawtooth");
+    while(lightVal < lightCal - sensitivity){
+      hertz = analogRead(A1);
+      if(hertz == 0) hertz = 1;
+      int soundPeak = 1023;
+      int delayTime = (1000000 / (hertz))/(soundPeak + 1);
+      Serial.println(delayTime);
+      
+      for(int i = 0; i <= soundPeak; i++){
+        analogWrite(DAC0, i);
+        delayMicroseconds(delayTime);
+      }
+      lightVal = analogRead(ph1);
+    }
+  }
+struct EnvelopeADSR
+{
+  double AttackTime;
+  double DecayTime;
+  double ReleaseTime;
+
+  double SustainAmplitude;
+  double StartAmplitude;
+
+  double TriggerOnTime;
+  double TriggerOffTime;
+
+  bool NoteOn;
+
+  EnvelopeADSR()
+  {
+    AttackTime = 0.01; //10ms
+    DecayTime = 0.01;
+    StartAmplitude = 1.0;
+    SustainAmplitude = 0.8;
+    ReleaseTime = 0.02;
+    TriggerOnTime = 0.0;
+    TriggerOffTime = 0.0;
+    NoteOn = false;
+    
+  }
+
+  double GetAmplitude(double mTime)
+  {
+    double Amplitude = 0.0;
+    double LifeTime = mTime - TriggerOnTime;
+
+    if(NoteOn)
+    {
+      //ADS
+
+      //Attack
+      if(LifeTime <= AttackTime)
+      {
+        Amplitude = (LifeTime / AttackTime) * StartAmplitude;
+      }
+    }
+    else
+    {
+      //R
+    }
+
+
+    
+    return Amplitude;
+  }
+
+  void NoteOn(double mTimeOn)
+  {
+    TriggerOnTime = mTimeOn;
+    NoteOn = true;
+  }
+
+  void NoteOff(double mTimeOff)
+  {
+    TriggerOffTime = mTimeOff;
+    NoteOff = false;
+  }
+  
+}
+
+  
+} 
+
+
+  
--- Main.ino
+++ Main.ino
@@ -1,85 +1,116 @@
+//final synthesizer codesss
+//XD lmao hahha epix gamers momemtns
+//finalcode.cpp
+
+/*
+    
+    Jace Bayless
+    Markus Becerra
+    Max Goad
+    Tri Pham
+    Apurva Rai
+    Minye Wu
+    
+*/
+
 #include <DueTimer.h>
 #include <SineWaveDue.h>
+#include "PlaySounds.h"
 
-int lightCal;
-int lightVal;
 
-int buttonState = 0;
-int lastButtonState = buttonState;
+bool *PHR = new bool[12];   // photoresistor value array
+bool *BS  = new bool [4];   // button value array
+int  *KN  = new int  [6];   // knob value array
 
-int state = 0; // 0 = square, 1 = sine, 2 = sawtooth
-const int ph1 = A0;
-int sensitivity = 100;
+int lightCal;   // PHR calibration value
+const int sensitivity = 100;    // PHR activation sensitivity
 
-int hertz = 400;
-double delayTime = 1000000 / (hertz * 2);
-int playHertz = hertz;
-      
-void setup()  { 
-  analogReadResolution(10);
-  analogWriteResolution(10);
-  Serial.begin(9600);
-  lightCal = analogRead(ph1);
-  pinMode(7, INPUT);
-  
+bool on = false;
+
+//main exec function for sounds
+PlaySounds exec;
+
+int STARTLEDPIN = 12;
+int STARTSWITCHPIN = 11;
+
+int STARTPHRPIN = A0;
+int STARTBSPIN = 0;
+int STARTKNPIN = A11;
+
+
+void updatePHR(bool* PHR)
+{
+    for(int rep = 0; rep< 12; rep++)
+    {
+        //something like this
+        PHR[rep] = (analogRead(rep + STARTPHRPIN) < (lightCal - sensitivity));
+        Serial.println(PHR[0]);
+     
+    }
 }
 
-void loop()  {
-  lightVal = analogRead(ph1);
-  buttonState = digitalRead(7);
-  hertz = analogRead(A1);
-  delayTime = 1000000 / (hertz * 2);
+void updateBS(bool* BS)
+{
+    for(int rep = 0; rep< 4; rep++)
+    {
+        //something like this
+        BS[rep] = digitalRead(STARTBSPIN + rep);
+    }
+}
 
-  // DETERMINE BUTTON PUSH
-  if(buttonState != lastButtonState){
-    if(buttonState == 1){
-      if(state == 2)
-        state = 0;
-      else
-        state++; 
+void updateKN(int* KN)
+{
+    for(int rep = 0; rep< 6; rep++)
+    {
+        //something like this
+        BS[rep] = analogRead(STARTKNPIN + rep);
     }
-    lastButtonState = buttonState;
-    delay(50);
-  }
-  
-  // PLAY SOUNDS
-  if(state == 0){
-    if(lightVal < lightCal - sensitivity){
-      analogWrite(DAC0, 1023);
-      delayMicroseconds(delayTime);
-      analogWrite(DAC0, 0);
-      delayMicroseconds(delayTime);
-    }
-  }
-  else if(state == 1){
-    if(lightVal < lightCal - sensitivity){
-      sw.playTone(hertz);
-      playHertz = hertz;
-      
-      while(lightVal < lightCal - sensitivity && hertz <= playHertz + 10 && hertz >= playHertz - 10){
-        lightVal = analogRead(ph1);
-        hertz = analogRead(A1);
-      }
-      sw.stopTone();
-    }
-  }
-  else if(state == 2){
-    Serial.println("Sawtooth");
-    while(lightVal < lightCal - sensitivity){
-      hertz = analogRead(A1);
-      if(hertz == 0) hertz = 1;
-      int soundPeak = 1023;
-      int delayTime = (1000000 / (hertz))/(soundPeak + 1);
-      Serial.println(delayTime);
-      
-      for(int i = 0; i <= soundPeak; i++){
-        analogWrite(DAC0, i);
-        delayMicroseconds(delayTime);
-      }
-      lightVal = analogRead(ph1);
-    }
-  }
-} 
+}
 
 
-  
+void setup()
+{
+    analogReadResolution(10);
+    analogWriteResolution(10);
+    Serial.begin(9600);
+
+    pinMode(STARTSWITCHPIN, INPUT_PULLUP); // sets start pin mode. i think PULLUP does automatic debouncing
+    pinMode(STARTLEDPIN, OUTPUT); // DECLARE THAT FIRST SUCKA AS OUTPUT !!!!!!!!!!!!
+    
+    lightCal = analogRead(STARTPHRPIN); //initialized light calibration value at first PHR
+    //maybe change this to an average of all of them at startup
+}
+
+void loop()
+{
+    //change switch pin
+    //on = digitalRead(STARTSWITCHPIN);
+    on = true;
+    if(on)
+    {
+        //ON led
+        digitalWrite(STARTLEDPIN, HIGH); 
+        
+        //update input values
+        updatePHR(PHR);
+        updateBS(BS);
+        updateKN(KN);
+        
+        //This exec function will update output values given the input
+        //output values : leds, aux, bluetooth     
+        exec.run(PHR, BS, KN);
+        
+        
+    }
+    
+    else if(!on)
+    {
+        //ON led
+        digitalWrite(STARTLEDPIN, LOW);
+
+        //stop will stop all sounds from being output. This essentially sets the volume to 0.
+        exec.stop();
+    }
+    
+    
+}
